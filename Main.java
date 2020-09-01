1e7c031883214af66478d7d3e125ca4095cdf053
Keith Smith <keith.smith@tvdsb.ca>
2020-09-01 17:33:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    int myNumber = 4; //sets number to 4
+    
+    if (myNumber >= 4){ //checks if number is greater than 4
+      System.out.println("Your number is greater than or equal to 4");
+    }
+    
+    else{ // else statement
+      System.out.println("Your number is not greater than nor is it equal to 4");
+    }
+  }
+}
