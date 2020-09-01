58a605778151db635e6ba28c05558ac93c89b3c6
Keith Smith <keith.smith@tvdsb.ca>
2020-09-01 17:41:14 +0000

    Updated to have marks out of 10

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
 class Main {
   public static void main(String[] args) {
 
-    int myNumber = 4; //sets number to 4
+    // This program gives a letter grade for a mark out of 10
+    int mark = 6; // sets mark to 6
+
+    if (mark < 5) { // checks if mark is less than 5
+      System.out.println("Sorry, you got an F, please try again");
+    }
+
+    else if (mark < 6) { // checks if mark is less than 6
+      System.out.println("You got a D");
+    }
+
+    else if (mark < 7) { // checks if mark is less than 7
+      System.out.println("You got a C");
+    } 
     
-    if (myNumber >= 4){ //checks if number is greater than 4
-      System.out.println("Your number is greater than or equal to 4");
+        else if (mark<8){ // checks if mark is less than 8
+      System.out.println("You got a B");
     }
     
-    else{ // else statement
-      System.out.println("Your number is not greater than nor is it equal to 4");
+    else if (mark<=10){ // checks if mark is less than or equal to 10
+      System.out.println("You gota A");
+    }
+
+    else { // else statement
+      System.out.println("The number you entered is invalid");
     }
   }
 }
